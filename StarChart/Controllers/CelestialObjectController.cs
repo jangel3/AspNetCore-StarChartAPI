f9bc199c7a05807d1442548d4f39cc8729d37f78
John Angel <john.angel@insperity.com[http]>
2021-06-17 14:35:23 -0500

    Added ability to create, update, delete and name change.

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -73,5 +73,66 @@ namespace StarChart.Controllers
 
             return Ok(celestialBodies);
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CelestialObject celestialObject)
+        {
+            var celestialEntity = _context.CelestialObjects.Add(celestialObject);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetById", new {celestialObject.Id}, celestialEntity.Entity);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, CelestialObject celestialObject)
+        {
+            var celestialBody = _context.CelestialObjects.FirstOrDefault(f => f.Id == id);
+            if (celestialBody == null)
+            {
+                return NotFound();
+            }
+
+            celestialBody.Name = celestialObject.Name;
+            celestialBody.OrbitalPeriod = celestialObject.OrbitalPeriod;
+            celestialBody.OrbitedObjectId = celestialObject.OrbitedObjectId;
+
+            _context.CelestialObjects.Update(celestialBody);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name)
+        {
+            var celestialBody = _context.CelestialObjects.Find(id);
+            if (celestialBody == null)
+            {
+                return NotFound();
+            }
+
+            celestialBody.Name = name;
+            _context.CelestialObjects.Update(celestialBody);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var celestialBodies = _context.CelestialObjects.Where(i => i.Id == id).ToList();
+            celestialBodies.AddRange(_context.CelestialObjects.Where(obj=> obj.OrbitedObjectId==id));
+
+            if (celestialBodies.Count == 0)
+            {
+                return NotFound();
+            }
+
+            _context.CelestialObjects.RemoveRange(celestialBodies);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
